f5493ba0693fbecfba21090edec0f03c40d35d69
Andrii <reyandrey98@gmail.com>
2022-11-15 15:33:39 +0200

    first step

--- src/src/js/01-gallery.js
+++ src/src/js/01-gallery.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+galleryItems.forEach((item) => {
+	const navGallery = document.querySelector(".gallery");
+	// console.log(navGallery);
+	const createGalleryItem = document.createElement("div");
+	createGalleryItem.classList.add("gallery__item");
+
+	const createGalleryLink = document.createElement("a");
+	createGalleryLink.classList.add("gallery__link");
+
+	const createGalleryImage = document.createElement("img");
+	createGalleryImage.classList.add("gallery__image");
+	createGalleryImage.setAttribute("src", item.preview);
+	createGalleryImage.setAttribute("data-source", item.original);
+	createGalleryImage.setAttribute("alt", item.description);
+
+	createGalleryLink.append(createGalleryImage);
+	createGalleryItem.append(createGalleryLink);
+	navGallery.append(createGalleryItem);
+});
